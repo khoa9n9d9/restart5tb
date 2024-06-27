b2833971b3183ad87d497fe0959e6d8e77b3c145
aohkkhoa <ndk1031999@gmail.com>
2024-06-27 11:04:43 +0700

    first  commit

---
+++ src/UserList.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import './UserList.css'; // Import CSS file
+
+const UserList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(
+                    'http://192.168.12.110:2030/api/Products'
+                );
+                const data = await response.json();
+                setProducts(data);
+            } catch (error) {
+                console.error('Error fetching product data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className="user-list-container">
+            <h1>Product List</h1>
+            <table className="product-table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Content</th>
+                        <th>Image</th>
+                        <th>Price</th>
+                        <th>Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map(product => (
+                        <tr key={product.id}>
+                            <td>{product.id}</td>
+                            <td>{product.name}</td>
+                            <td>{product.content}</td>
+                            <td><img src={product.image1} alt={product.name} className="product-image" /></td>
+                            <td>${product.price}</td>
+                            <td><a href={product.url} className="product-link" target="_blank" rel="noopener noreferrer">View Product</a></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default UserList;
